bd2cdf16c64fce1d0ce49066d3a4f88fde524108
Lucas <lucas@openix.com.ar>
2021-08-16 19:22:25 -0300

    Add logic to know if there is equals

--- TikTakToe/src/scenes/Play.js
+++ TikTakToe/src/scenes/Play.js
@@ -7,7 +7,8 @@ class Play extends Phaser.Scene{
     }
 
     init(){
-        this.turn = true // si es true el jugardor cero esta iniciando sino es el jugador cero
+        this.turn = Phaser.Math.Between(0, 1) // si es true el jugardor cero esta iniciando sino es el jugador cero
+        this.changeTurn();
         this.canvasCenter = {
             width: this.sys.game.config.width / 2, // obtenemos la anchura total del canvas y lo dividmos en 2
             height: this.sys.game.config.height / 2, // obtenemos el alto y lo dividimos en 2
@@ -21,19 +22,21 @@ class Play extends Phaser.Scene{
         this.table.setAlpha(0);
         // agregar los botones para saber la posicion
         this.tableButtonPosition = [
-            this.add.image(this.canvasCenter.width, this.canvasCenter.height, "position"),
-            this.add.image(this.canvasCenter.width + 48, this.canvasCenter.height, "position"),
-            this.add.image(this.canvasCenter.width + 48, this.canvasCenter.height + 48, "position"),
-            this.add.image(this.canvasCenter.width, this.canvasCenter.height + 48, "position"),
-            this.add.image(this.canvasCenter.width - 48, this.canvasCenter.height - 48, "position"),
-            this.add.image(this.canvasCenter.width, this.canvasCenter.height - 48, "position"),
-            this.add.image(this.canvasCenter.width - 48, this.canvasCenter.height, "position"),
-            this.add.image(this.canvasCenter.width - 48, this.canvasCenter.height + 48, "position"),
-            this.add.image(this.canvasCenter.width + 48, this.canvasCenter.height - 48, "position"),
+            this.add.image(this.canvasCenter.width - 48, this.canvasCenter.height - 48, 'position'),
+            this.add.image(this.canvasCenter.width, this.canvasCenter.height - 48, 'position'),
+            this.add.image(this.canvasCenter.width + 48, this.canvasCenter.height - 48, 'position'),
+
+            this.add.image(this.canvasCenter.width - 48, this.canvasCenter.height, 'position'),
+            this.add.image(this.canvasCenter.width, this.canvasCenter.height, 'position'),
+            this.add.image(this.canvasCenter.width + 48, this.canvasCenter.height, 'position'),
+
+            this.add.image(this.canvasCenter.width - 48, this.canvasCenter.height + 48, 'position'),
+            this.add.image(this.canvasCenter.width, this.canvasCenter.height + 48, 'position'),
+            this.add.image(this.canvasCenter.width + 48, this.canvasCenter.height + 48, 'position'),
         ]
 
         // todos los botones estan en un array, le agregamos interactividad
-        this.tableButtonPosition.map( button => {
+        this.tableButtonPosition.map( (button, index) => {
             button.setInteractive();
             button.setAlpha(0.5)
             // cuando el mouse pase por encima del boton se cambiar la imagen a opaco
@@ -54,8 +57,10 @@ class Play extends Phaser.Scene{
                 // cuando hacemos click no se puede volver a tocar esa posicion entonces le sacamos el interactivo
                 button.removeInteractive();
                 button.frame = this.textures.getFrame(`${db.player}`)
+                this.logic(index);
                 this.turn = !this.turn;
                 db.player = this.turn ? "cero" : "equis";
+                console.log(db.table);
 
             })
         })
@@ -66,6 +71,40 @@ class Play extends Phaser.Scene{
             ease: "Power1",
         });
 
+    }
+
+    logic(index){
+        const bi = this.getBidimensional(index);
+        db.table[bi.y][bi.x] = this.turn ? 1 : 0;
+
+        if (this.win().equals){
+            alert("Empate");
+        }
+    }
+
+    getBidimensional(index){
+        return {
+            y: Math.floor(index / 3),
+            x: index % 3,
+        };
+    }
+
+    changeTurn(){
+        this.turn = !this.turn;
+        db.player = this.turn ? "cero" : "equis";
+    }
+
+    win(){
+        const output = {
+            win: false,
+            equals: false,
+            player: db.player,
+        };
+        if (!db.table.flat().some(x => x === -1)){
+            output.equals = true;
+        }
+
+        return output;
     }
 
 
